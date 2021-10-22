7c01d8fe74e3d7b5c979387e61f2998a7fd9d9d3
BrunoEnriqueB <brunobaronenrique@gmail.com>
2021-10-22 16:32:21 -0300

    Finish Class 1 + Create user profile

--- src/routes.ts
+++ src/routes.ts
@@ -3,13 +3,16 @@ import { AuthenticateUserController } from "./controllers/AuthenticateUserContro
 import { CreateMessageController } from "./controllers/CreateMessageController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
+import { ProfileUserController } from "./controllers/ProfileUSerController";
 
 const router = Router();
 
-router.post("/authenticate", new AuthenticateUserController().handle);
+router.post("/authenticate", new AuthenticateUserController().handle); //válida o usuário
 
-router.post("/messages", ensureAuthenticated, new CreateMessageController().handle);
+router.post("/messages", ensureAuthenticated, new CreateMessageController().handle); //envia mensagem
 
-router.get("/messages/last3", new GetLast3MessagesController().handle)
+router.get("/messages/last3", new GetLast3MessagesController().handle); //pega as ultimas 3 mensagens
+
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle); //devolve as informações do usuário
 
 export {router}
---
+++ src/services/ProfileUserService.ts
@@ -0,0 +1,15 @@
+import prismaClient from "../prisma"
+
+class ProfileUserService {
+    async execute(user_id: string) { // Devolve as últimas 3 mensagens do usuário
+        const user = await prismaClient.user.findFirst({ //acha o usuário de acordo com seu id
+            where: {
+                id: user_id
+            }
+        })
+
+        return user; //devolve o usuário
+    };
+};
+
+export {ProfileUserService};
